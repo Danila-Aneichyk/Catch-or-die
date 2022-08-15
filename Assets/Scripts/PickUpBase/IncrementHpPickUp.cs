1673a114826d9158df2fd2e2bba7d43f8ec07226
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-08-16 01:32:45 +0300

    Add decremen hp pickUp.

--- Assets/Scripts/PickUpBase/IncrementHpPickUp.cs
+++ Assets/Scripts/PickUpBase/IncrementHpPickUp.cs
@@ -4,6 +4,11 @@ public class IncrementHpPickUp : PickUpBase
 {
     protected override void ApplyEffect(Collision2D col)
     {
+        Statistics.Instance.CurrentHp--; 
         
+        if (Statistics.Instance.CurrentHp <= 0)
+        {
+            //TODO: GameOver 
+        }
     }
 }
